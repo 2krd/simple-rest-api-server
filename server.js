aa26a75cde9a6bf556ba569b0b2b1e2579e811e2
Khang Dinh <thesephi@gmail.com>
2015-09-13 15:58:29 +0700

    Added CORS and support for Ionic framework

--- server.js
+++ server.js
@@ -21,7 +21,24 @@ function callAPI(req, res, next) {
   });
 }
 
-var server = restify.createServer();
+server.use(restify.CORS()); // consider disabling this on production
+server.use(restify.queryParser()); // note the order matters (queryParser, then bodyParser)
+server.use(restify.bodyParser());
+function corsHandler(req, res, next) {
+  res.setHeader('Access-Control-Allow-Headers', 'Accept, Access-Control-Allow-Credentials, Access-Control-Allow-Headers, Access-Control-Allow-Methods, Access-Control-Allow-Origin, Content-Type');
+  res.setHeader('Access-Control-Allow-Methods', '*'); // can tweak to specific HTTP Verbs if needed
+  // these are referenced from https://github.com/restify/node-restify/issues/296#issuecomment-12333568
+  // and left here just for reference purposes
+  //res.setHeader('Access-Control-Allow-Origin', '*');
+  //res.setHeader('Access-Control-Expose-Headers', 'X-Api-Version, X-Request-Id, X-Response-Time');
+  //res.setHeader('Access-Control-Max-Age', '1000');
+  return next();
+}
+function optionsRoute(req, res, next) {
+  res.send(200); // let's be easy here and make way for OPTIONS requests to go through
+  return next();
+}
+server.opts('/\.*/', corsHandler, optionsRoute);
 server.get('/hello/:name', respond);
 server.head('/hello/:name', respond);
 server.get('/api/:func', callAPI);
